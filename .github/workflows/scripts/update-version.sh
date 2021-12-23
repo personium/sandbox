3674ca4c91c71227629e7ddcd39c35b0ff1abf34
ishiguro-yo <4243145+yoh1496@users.noreply.github.com>
2021-12-23 05:29:51 +0000

    brush up release codes

--- .github/workflows/scripts/update-version.sh
+++ .github/workflows/scripts/update-version.sh
@@ -2,45 +2,28 @@
 
 echo 'Update version started.'
 
-# Check git version
-git checkout master
-
-MINOR_VERSION=$(
-  sed -n 's|^    <version>[0-9]\+\.[0-9]\+\.\([0-9]\+\)</version>|\1|p' pom.xml
-)
-if [ -z "$MINOR_VERSION" ]; then
-  echo 'Cannot get version.'
-  exit 1
-fi
-
-MINOR_VERSION=$((++MINOR_VERSION))
-
 # Rebase develop onto master branch after remove -SNAPSHOT
 git checkout develop
 git rebase master
 
 # update version in pom.xml
-sed -i \
- "s|^\(    <version>[0-9]\+\.[0-9]\+\.\)[0-9]\+\(</version>\)|\1${MINOR_VERSION}-SNAPSHOT\2|" \
- pom.xml
+mvn -B versions:set -DnextSnapshot && mvn -B versions:commit
+
+NEXT_RELEASE_VERSION=$(
+  mvn -q versions:set -DremoveSnapshot && mvn -q help:evaluate -Dexpression=project.version -DforceStdout && mvn -q versions:revert
+)
 
 if [ "${COMPONENT}" = "personium-core" -o "${COMPONENT}" = "personium-engine" ]; then
   # update version in personium-unit-config-default.properties
   sed -i \
-    "s|^\(io\.personium\.core\.version=[0-9]\+\.[0-9]\+\.\)[0-9]\+|\1${MINOR_VERSION}|" \
+    "s|^\(io\.personium\.core\.version=\).*\$|\1${RELEASE_VERSION}|" \
     src/main/resources/personium-unit-config-default.properties
 fi
 
 # Git commit and push
-
-VERSION=$(
-  sed -n 's|^    <version>\([0-9]\+\.[0-9]\+\.[0-9]\+-SNAPSHOT\)</version>|\1|p' pom.xml
-)
-echo ${VERSION}
-
 git diff
 git add .
-git commit -m "Update to v${VERSION}"
+git commit -m "Update to v${NEXT_RELEASE_VERSION}"
 git push origin develop
 
 echo 'Suceeded!'
